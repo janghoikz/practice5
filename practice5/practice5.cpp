d0c0e863c642e98d4ef6ab5f7b7be43c67c899f3
ohne <ghdlr785@gmail.com>
2022-03-29 15:09:24 +0900

    초기 커밋.

---
+++ practice5/practice5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+class Student 
+{
+    private : 
+        int score;
+
+    public :
+        int number;
+    private : 
+        void Ranking()
+        {
+        }
+        void Print()
+        {
+            std::cout << score << std::endl;
+        }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
